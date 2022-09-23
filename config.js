a83d53d1b655be166df4f84a81656aa33264d973
janjov <114235006+janjov@users.noreply.github.com>
2022-09-23 20:44:25 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "For Crush",
+    introTitle: 'Hello Crush',
+    introDesc: 'I have little message for you hehe',
+    btnIntro: 'Click',
+    title: 'Kamusta ka? I hope you are doing well hehe',
+    desc: 'Crush mo ba ako?',
+    btnYes: 'Yes',
+    btnNo: 'No',
+    question: 'Di nga? Crush mo ako?',
+    btnReply: 'Kinikilig ako HAHA',
+    reply: 'Crush din kita HAHA',
+    mess: 'Di ko alam sasabihin ko HAHA',
+    messDesc: 'Speechless na ako wala na',
+    btnAccept: 'Yun lang sana magustuhan mo HEHE',
+    messLink: 'https://github.kiiiii lun <3com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
